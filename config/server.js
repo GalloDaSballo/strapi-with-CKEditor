011c1b9e5e8ddd3b7c87676ad982b73240dce090
Entreprenerd <alex@entreprenerd.xyz>
2020-07-29 15:44:31 +0200

    Initial Commit with Plugin Folder

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd520abc080c0d2d22253201de43cacc4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2bb2ff23-97a1-4d56-97c7-9419ba41201a'
+};
